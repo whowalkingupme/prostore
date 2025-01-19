4d8370fef161c91beae6e3f9dfdf9feaf171e800
eric <whowalkingupme@gmail.com>
2025-01-19 21:15:22 +0800

    2025/01/19

---
+++ components/shared/header/modo-toggle.tsx
@@ -0,0 +1,61 @@
+'use client';
+import { useState, useEffect } from 'react';
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuContent,
+    DropdownMenuCheckboxItem,
+    
+} from '@/components/ui/dropdown-menu';
+import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
+
+const ModeToggle = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => setMounted(true), []);
+
+    if (!mounted) return null;
+
+    return (<DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="ghost"
+                className='focus-visible:ring-0 focus-visible:ring-offset-0'>
+                {
+                    theme === 'system' ? (
+                        <SunMoon />
+                    ) : theme === 'dark' ? (
+                        <MoonIcon />
+                    ) : (
+                        <SunIcon />
+                    )
+                }
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem
+                checked={theme === 'system'}
+                onClick={() => setTheme('system')}>
+                System
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+                checked={theme === 'dark'}
+                onClick={() => setTheme('dark')}>
+                Dark
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem
+                checked={theme === 'light'}
+                onClick={() => setTheme('light')}>
+                Light
+            </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+    </DropdownMenu>);
+}
+
+export default ModeToggle;
